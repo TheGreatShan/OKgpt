f5d515c3e3940dd6d58ed78526a6b87d71915959
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-08-14 16:52:39 +0200

    test click me button

--- app/src/main/java/ch/shan/okgpt/MainActivity.kt
+++ app/src/main/java/ch/shan/okgpt/MainActivity.kt
@@ -1,17 +1,25 @@
 package ch.shan.okgpt
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        TestClicked(this)
     }
 
-    fun TestClicked(view: View) {
-        view.findViewById<TextView>(R.id.textView).visibility = View.VISIBLE
+    fun TestClicked(context: Context) {
+        val btn_click_me: Button = findViewById(R.id.button)
+        btn_click_me.setOnClickListener {
+            Toast.makeText(context, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
